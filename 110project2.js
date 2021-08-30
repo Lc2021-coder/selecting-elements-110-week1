faf3869d66ede3aff435fb893e5fcaec8464cdc2
Lc2021-coder <81456231+Lc2021-coder@users.noreply.github.com>
2021-08-30 02:37:52 -0700

    Add files via upload

---
+++ 110project2.js
@@ -0,0 +1,23 @@
+<script>
+
+
+function dis(val){
+
+document.getElementById("calculator-grid").value+=val & nbsp
+}
+function solve()
+
+ x=document.getElementById("My-Calculator").value{
+}
+y=eval(x){
+document.getElementById("calculator-grid").value=y
+}
+	
+{
+function clr()
+
+{
+document.getElementById("calculator-grid").value=""
+}
+}
+</script>
